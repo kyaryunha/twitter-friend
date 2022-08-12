9e91e43dec1cc9001041f43918c746d816bb3dba
Shin Hyun <kyaryunha@naver.com>
2022-08-12 18:01:51 +0900

    fix ga tag production mode 일 때만

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,23 +8,26 @@ export default class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    {/* Global Site Tag (gtag.js) - Google Analytics */}
-                    <script
-                        async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-                    />
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
+                    {
+                        process.env.NODE_ENV === "production" &&
+                        <>
+                            {/* Global Site Tag (gtag.js) - Google Analytics */}
+                            <script
+                                async
+                                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                            />
+                            <script
+                                dangerouslySetInnerHTML={{
+                                    __html: `
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', '${GA_TRACKING_ID}', {
       page_path: window.location.pathname,
     });
-  `,
-                        }}
-                    />
+  `}} />
+                        </>
+                    }
                 </Head>
                 <body>
                 <Main />
