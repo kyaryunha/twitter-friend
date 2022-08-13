c8bbc00a22d481963135cfec6279fd80f0d4211e
Shin Hyun <kyaryunha@naver.com>
2022-08-14 00:32:24 +0900

    typo placeholder update

--- components/create-template/questions/question-likes.tsx
+++ components/create-template/questions/question-likes.tsx
@@ -9,7 +9,7 @@ const QuestionTwitterNicknameAndId: FC<QuestionsStoreTypes>  = observer(({store}
                     좋아하는 것을 적어주세요!
                 </StyledQuestion>
                 <StyledLabel>
-                    <StyledTextarea placeholder="프론트엔드, 벡엔드 (길어지면 엔터를 눌러주세요)" value={store.likes} onChange={(e) =>store.updateLikes(e.target.value)} />
+                    <StyledTextarea placeholder="프론트엔드, 벡엔드 (길게 쓰면 겹칠수 있으니, 내용을 줄이거나 엔터를 눌러주세요)" value={store.likes} onChange={(e) =>store.updateLikes(e.target.value)} />
                 </StyledLabel>
             </>
         )
